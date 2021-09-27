1fce2a5acfcada4dcc561f17e4388375486015db
samisan1122 <samisan1122@gmail.com>
2021-09-27 14:03:57 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 process.env.NTBA_FIX_319 = 1;
 const TelegramBot = require('node-telegram-bot-api');
 const {NearTime} = require('./near');
+const fs = require('fs');
 
-const token =  process.env.TELEGRAM_TOKEN || '1902658014:AAGo4EPGOQUyu-Fa1VfwjaC7bC-tZIyd9IE';
+fs.readFile('token.txt', 'utf8', function(err, token) {
+  if (err) throw err;
+  const bot = new TelegramBot(token, {polling: true});
 
-const bot = new TelegramBot(token, {polling: true});
-
-bot.on('message', (msg) => {
-  if(msg.text.toString().toLowerCase().indexOf('neartime') === 0 && msg.text.toString().length === 8) {
-    NearTime().then((resp) => bot.sendMessage(msg.chat.id, resp));
-  }
+  bot.on('message', (msg) => {
+    if(msg.text.toString().toLowerCase().indexOf('neartime') === 0 && msg.text.toString().length === 8) {
+      NearTime().then((resp) => bot.sendMessage(msg.chat.id, resp));
+    }
+  });
 });
